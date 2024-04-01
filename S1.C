1c667c5c9e51c03f821d9868410fc758dae0f053
shaiwalsharma15 <154620324+shaiwalsharma15@users.noreply.github.com>
2024-04-01 23:18:25 +0530

    Create S1.C

---
+++ S1.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+int num1=10,num2=2;
+int num3;
+    printf("the first number is %d\n", num1);
+    printf("the second number is %d\n", num2);
+   
+    num3= num1+num2;
+    printf("the sum of two number is %d\n", num3);
+   
+    num3=num1-num2;
+    printf("the difference of two number is %d\n", num3);
+    
+    num3=num1*num2;
+    printf("the product of two number is %d\n", num3);
+    
+    num3=num1/num2;
+    printf("the division of two number is %d", num3);
+    
+    return 0;
+}
