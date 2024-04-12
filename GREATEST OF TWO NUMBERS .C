8147287f8357af00d1eb9756d5a46d1b77b82e4a
shaiwalsharma15 <154620324+shaiwalsharma15@users.noreply.github.com>
2024-04-13 00:16:56 +0530

    GREATEST OF TWO NUMBERS .C

---
+++ GREATEST OF TWO NUMBERS .C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    printf("the first number is \n");
+    scanf("%d", &a);
+    printf("the second number is \n");
+    scanf("%d", &b);
+if (a<b)
+printf("the greater number is %d",b);
+else
+printf("the greater number is%d",a);
+   
+    return 0;
+}
