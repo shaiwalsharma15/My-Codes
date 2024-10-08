f1fc92f7239c7243e99cd82519472d43fe0dcd38
shaiwalsharma15 <154620324+shaiwalsharma15@users.noreply.github.com>
2024-10-09 00:55:24 +0530

    Create Program that defines a class with two methods: getString() to take input from the console and printString() to print the string in uppercase.py

---
+++ Program that defines a class with two methods: getString() to take input from the console and printString() to print the string in uppercase.py
@@ -0,0 +1,10 @@
+class StringManipulator:
+    def __init__(self):
+        self.string = ""
+def getString(self):
+        self.string = input("Enter a string: ")
+  def printString(self):
+        print(self.string.upper())
+str_obj = StringManipulator()
+str_obj.getString()
+str_obj.printString()
